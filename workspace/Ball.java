ae7a20e1cde919f890166aa19138933d79b84c61
Noodul1 <aspiderlickedme15@gmail.com>
2024-12-08 21:54:29 +0000

    commit -m “submission 12/8”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,13 +1,87 @@
+//  Class author:  Jay Vedamony
+//  Date created:  12/03/2024
+//  General description: methods for creating and moving ball in brick breaker
+
 import java.awt.*;
 
 public class Ball {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
+	private int xPos;
+
+ 	private int yPos;
+
+	private int size;
+
+	private int xVelocity;
+
+	private int yVelocity;
 	
 	//constructor(s):
-	
+	Ball(int x , int y, int r) {
+		xPos = x;
+		yPos = y;
+		size = r;
+		xVelocity = 1;
+		yVelocity = -1;
+		}
+		
 	//methods:
 
+	// pre-condition: ball collides brick, paddle, or edge of screen
+	// post-condition: negates x velocity 
+	public void reverseX(){
+		xVelocity = -xVelocity;
+	}
+	// pre-condition: ball collides brick, paddle, or edge of screen
+	// post-condition: negates y velocity
+	public void reverseY(){
+		yVelocity = -yVelocity;
+	}
+
+	// pre-condition: game started and is running
+	// post-condition: ball moves, corresponding with the set velocities
+	public void move(){
+		
+		xPos += xVelocity;
+		yPos += yVelocity;
+		
+	}
+
+	// pre-condition: ball object is not null
+	// post-condition: draws ball, corresponding with set size and position
+	public void draw (Graphics g){
+		g.setColor(Color.GREEN);
+		g.fillOval(xPos, yPos, size, size);
+	}
+
+	// getters and setters
+	public void setX(int x){
+		xPos = x;
+	}
+
+	public int getXpos(){
+		return xPos;
+	}
+
+	public void setY(int y){
+		yPos = y;
+	}
+
+    public int getYpos(){
+		return yPos;
+	}
+
+	public int getSize(){
+		return size;
+	}
+
+	public void setXVelocity(int v){
+		xVelocity = v;
+	}
+
+    public void setYVelocity(int v){
+		yVelocity = v;
+	}
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,3 +1,7 @@
+//  Class author:  Jay Vedamony
+//  Date created:  12/03/2024
+//  General description: methods for creating and moving paddle in brick breaker
+
 import java.awt.Graphics;
 import java.awt.*;
 
@@ -6,11 +10,85 @@ public class Paddle {
 	//your code here!
 	
 	//don't forget you need instance variables:
-	
-	
+	private int xPos;
+
+	private int yPos;
+
+	private int width;
+
+	private int height;
+
+	private int velocity;
+
 	//constructor(s):
-	
+	Paddle(int x , int y, int w, int h){
+		xPos = x;
+		yPos = y;
+		width = w;
+		height = h;
+		velocity = 0;
+}
 	//methods:
 	
+	//pre-condition: right or left key is pressed
+	//post-condition: increases or decreases velocity
+	public void addVelocity(int v){
+		if(velocity >= 10){
+			velocity = -10;
+		} else if(velocity <= -10){
+			velocity = -10;
+		} else {
+			velocity += v;
+		}
+	}
+
+	// pre-condition: game started and is running
+	// post-condition: paddle moves
+	public void move(){
+		if(velocity > 2){
+			velocity = 2;
+		} else if(velocity < -2){
+			velocity = -2;
+		} else{
+		xPos += velocity;
+		}
+	}
+	
+	// pre-condition: paddle object is not null
+	// post-condition: draws paddle, corresponding with set size and position
+	public void draw (Graphics g){
+		g.setColor(Color.MAGENTA);
+		g.fillRect(xPos, yPos, width, height);
+	}
+
+
+	// getters and setters
+	public void setX(int x){
+		xPos = x;
+	}
+
+	public int getX(){
+		return xPos;
+	}
+
+	public int getY(){
+		return yPos;
+	}
+
+	public int getWidth(){
+		return width;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+
+	public void setVelocity(int v){
+		velocity = v;
+	}
+
+	public int getVelocity(){
+		return velocity;
+	}
 	
 }
